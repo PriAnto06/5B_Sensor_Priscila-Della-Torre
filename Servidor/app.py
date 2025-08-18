9b171368a88be5875a994c930e14a7b132951f5c
PriAnto06 <priscilaantonella.d.07@gmail.com>
2025-08-18 11:31:15 -0300

    hice un metodo post poara usarlo en ESP32

--- Servidor/app.py
+++ Servidor/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+#json
+#{
+   # "nombre":"luxometro",
+    #"valor": "135"
+#}
+
+@app.route("/api/sensor",methods=["POST"])
+def sensor():
+    servidor = request.json
+    nombre = servidor.get["luxometro"]
+    valor = servidor.get ["135"]
+    print(f"Nombre: {servidor['nombre']} - Valor: {servidor['valor']}")
+    return "OK"
