8ef8b12f01a76d6c0348c9cfeaf436e755c3fbd9
PriAnto06 <priscilaantonella.d.07@gmail.com>
2025-08-25 10:56:54 -0300

    hicimos que la respuesta sea un json

--- Servidor/app.py
+++ Servidor/app.py
@@ -1,10 +1,31 @@
-from flask import Flask,request
+from flask import Flask,g, jsonify, request, url_for
+from math import ceil
+import sqlite3
+def dict_factory(cursor, row):
+  """Arma un diccionario con los valores de la fila."""
+  fields = [column[0] for column in cursor.description]
+  return {key: value for key, value in zip(fields, row)}
+
+def abrirConexion():
+   if 'db' not in g:
+      g.db = sqlite3.connect("datos3.sqlite")
+      g.db.row_factory = dict_factory
+   return g.db
+def cerrarConexion(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 
 app = Flask(__name__)
+app.teardown_appcontext(cerrarConexion)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/api/test")
+def test():
+    return "funcionando!"
+
+#@app.route("/")
+#def hello_world():
+ #   return "<p>Hello, World!</p>"
 
 #json
 #{
@@ -15,7 +36,14 @@ def hello_world():
 @app.route("/api/sensor",methods=["POST"])
 def sensor():
     servidor = request.json
-    nombre = servidor.get["luxometro"]
-    valor = servidor.get ["135"]
-    print(f"Nombre: {servidor['nombre']} - Valor: {servidor['valor']}")
-    return "OK"
+    nombre = servidor['nombre']
+    valor = servidor['valor']
+    print(f"Nombre: {nombre} - Valor: {valor}")
+    db = abrirConexion()
+    db.execute("INSERT INTO ESP32(Nombre,Valor) VALUES(?,?)", (nombre,valor))
+    db.commit()
+    datos = { "resultado": "Ok",
+             "nombre": nombre,
+             "valor": valor }
+    cerrarConexion()
+    return datos
