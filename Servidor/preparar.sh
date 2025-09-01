87a2d4867b2f5bd68dbaddc528ed0fcefe11b270
PriAnto06 <priscilaantonella.d.07@gmail.com>
2025-09-01 08:53:27 -0300

    Aregle cosas

--- Servidor/preparar.sh
+++ Servidor/preparar.sh
@@ -3,12 +3,10 @@ python -m venv .venv
 source .venv/bin/activate
 #instalar el flask
 pip install flask
-#para ver la version
-flask --version
+#crea la base de datos
+sqlite3 datos3.sqlite < base de datos.sql
 #para correr
 flask run --debug
-#crear la base de datos
-sqlite3 datos3.sqlite < base de datos.sql
 #para ejecutar la base de datos
-source preparar.sh
+#source preparar.sh
 # opcion 2 .preparar.sh
